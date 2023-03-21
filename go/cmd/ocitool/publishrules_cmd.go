d6bcc8c0e6125cd05e0f1f9f852a0512d8dec646
Andrew Bayer <andrew.bayer@gmail.com>
2023-03-21 12:03:01 -0400

    Move cmd, internal, and pkg into a new Go module
    This lets us depend on `github.com/DataDog/rules_oci/go` in other projects without Gazelle getting confused by depending on the Go module `github.com/DataDog/rules_oci`, which Gazelle's update-repos turns into the label `@com_github_datadog_rules_oci`, and depending on the Bazel rules from this repo, which is named `@com_github_datadog_rules_oci`.
    
    The `go.work` is just a way to have a `--from_file` for `gazelle --update-repos` that pulls in both `./go.mod` and `./go/go.mod`. This way, we can have just one `gazelle --update-repos` call that pulls in every Go dependency for both modules properly.
    
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

---
+++ go/cmd/ocitool/publishrules_cmd.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DataDog/rules_oci/go/pkg/ociutil"
+
+	ocispecv "github.com/opencontainers/image-spec/specs-go"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/urfave/cli/v2"
+)
+
+func PublishRulesCmd(c *cli.Context) error {
+	resolver := ociutil.DefaultResolver()
+
+	desc, err := resolver.PushBlob(c.Context, c.String("file"), c.String("ref"), "application/x.datadog.bazel.rules.tar+gz")
+	if err != nil {
+		return fmt.Errorf("failed to push blob: %w", err)
+	}
+
+	manifest := ocispec.Manifest{
+		Versioned: ocispecv.Versioned{
+			SchemaVersion: 2,
+		},
+		MediaType: ocispec.MediaTypeImageManifest,
+		Config:    desc,
+		Annotations: map[string]string{
+			"org.opencontainers.image.source": "github.com/DataDog/rules_oci",
+			"target":                          "prod",
+		},
+	}
+
+	_, err = resolver.MarshalAndPushContent(c.Context, c.String("ref"), manifest, manifest.MediaType)
+	if err != nil {
+		return err
+	}
+
+	url, err := ociutil.DescriptorToURL(c.String("ref"), desc)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Pushed: %v", url)
+
+	return nil
+}
