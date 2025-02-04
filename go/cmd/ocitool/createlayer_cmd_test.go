4c1ed5b24297399358a06f46e72fa7ca5ade9b5a
Brian Myers <brian.myers@datadoghq.com>
2025-02-03 19:03:24 -0600

    fix: fix modes and owners

--- go/cmd/ocitool/createlayer_cmd_test.go
+++ go/cmd/ocitool/createlayer_cmd_test.go
@@ -10,9 +10,10 @@ func TestParseIntWorksAsExpected(t *testing.T) {
 		input    string
 		expected int64
 	}{
-		{input: "0x755", expected: 0x755},
-		{input: "1877", expected: 0x755},
-		{input: "0b11101010101", expected: 0x755},
+		{input: "0o755", expected: 0o755},
+		{input: "0b111101101", expected: 0o755},
+		{input: "493", expected: 0o755},
+		{input: "0x1ed", expected: 0o755},
 	} {
 		actual, err := strconv.ParseInt(tc.input, 0, 64)
 		if err != nil {
